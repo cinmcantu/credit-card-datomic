783a0007ba4c3dbd76fed0ae1970f9e41bbcc2d1
cinmcantu <cinthia.cantu@nubank.com.br>
2021-12-10 17:33:10 -0300

    Creating specifics functions to add clients and cards

--- src/credit_card_datomic/client.clj
+++ src/credit_card_datomic/client.clj
@@ -1,6 +1,7 @@
 (ns credit-card-datomic.client
   (:require [schema.core :as s]
-            [credit-card-datomic.credit-card :as c.credit-card]))
+            [credit-card-datomic.credit-card :as c.credit-card]
+            [credit-card-datomic.db :as c.db]))
 
 (s/set-fn-validation! true)
 
@@ -11,9 +12,31 @@
              (s/optional-key :client/cpf)  s/Str
              (s/optional-key :client/card) [c.credit-card/Card]})
 
+(def client-schema [{:db/ident       :client/id
+                     :db/valueType   :db.type/uuid
+                     :db/cardinality :db.cardinality/one
+                     :db/unique      :db.unique/identity}
+                    {:db/ident       :client/name
+                     :db/valueType   :db.type/string
+                     :db/cardinality :db.cardinality/one}
+                    {:db/ident       :client/cpf
+                     :db/valueType   :db.type/string
+                     :db/cardinality :db.cardinality/one}
+                    {:db/ident       :client/card
+                     :db/valueType   :db.type/ref
+                     :db/cardinality :db.cardinality/many}])
+
+(c.db/create-schema! client-schema)
+
 (s/defn new-client :- Client
   [name cpf]
   {:client/id   (uudi)
    :client/name name
    :client/cpf  cpf
    :client/card []})
+
+(s/defn add-clients!
+  [clients :- [Client]]
+  (c.db/upsert-on-db! clients))
+
+
--- src/credit_card_datomic/credit_card.clj
+++ src/credit_card_datomic/credit_card.clj
@@ -1,5 +1,8 @@
 (ns credit-card-datomic.credit-card
-  (:require [schema.core :as s]))
+  (:use clojure.pprint)
+  (:require [schema.core :as s]
+            [credit-card-datomic.db :as c.db]
+            [datomic.api :as d]))
 
 (s/set-fn-validation! true)
 
@@ -10,12 +13,38 @@
            (s/optional-key :card/cvv)             Long
            (s/optional-key :card/expiration-date) s/Str})
 
+(def card-schema [{:db/ident       :card/id
+                   :db/valueType   :db.type/uuid
+                   :db/cardinality :db.cardinality/one
+                   :db/unique      :db.unique/identity}
+                  {:db/ident       :card/number
+                   :db/valueType   :db.type/string
+                   :db/cardinality :db.cardinality/one}
+                  {:db/ident       :card/expiration-date
+                   :db/valueType   :db.type/string
+                   :db/cardinality :db.cardinality/one}
+                  {:db/ident       :card/cvv
+                   :db/valueType   :db.type/long
+                   :db/cardinality :db.cardinality/one}])
+
+(c.db/create-schema! card-schema)
+
 (s/defn new-card :- Card
   [number cvv exp-date]
   {:card/id              (uudi)
    :card/number          number
    :card/cvv             cvv
    :card/expiration-date exp-date})
+
+(s/defn add-credit-cards!
+  [cards :- [Card]]
+  (c.db/upsert-on-db! cards))
+
+
+
+
+
+
 
 
 
